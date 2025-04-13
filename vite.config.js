66324975323a6e2175737ae79197f710b61791c6
Aksharass <113843078+Aksharass@users.noreply.github.com>
2025-04-13 11:26:36 +0530

    Add _redirects

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // ensures public/_redirects is copied
 })
